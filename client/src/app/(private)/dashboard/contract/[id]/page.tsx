47a5d1565d852314647eb71040e9e88ab4c44900
tusharranjan007 <92373617+tusharranjan007@users.noreply.github.com>
2024-11-06 05:03:35 -0500

    fix  3

--- client/src/app/(private)/dashboard/contract/[id]/page.tsx
+++ client/src/app/(private)/dashboard/contract/[id]/page.tsx
@@ -3,11 +3,12 @@
 import ContractResults from "./_components/contract-results";
 
 interface IContractResultsProps {
-  params: { id: string };
+  params: { id: string } | unknown;
 }
 
 export default function ContractPage({
-  params: { id },
+  params,
 }: IContractResultsProps) {
+  const { id } = params as { id: string };  // Cast params to the expected type
   return <ContractResults contractId={id} />;
 }
