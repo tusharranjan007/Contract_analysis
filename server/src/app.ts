433c9c88452875f5f1619632342e3b529e766abd
tusharranjan007 <92373617+tusharranjan007@users.noreply.github.com>
2024-11-06 20:33:41 -0500

    fix 10

--- server/src/app.ts
+++ server/src/app.ts
@@ -28,6 +28,8 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
