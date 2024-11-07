ef3cdd58b0b257c9de1e6b20be921114fee6ad3d
tusharranjan007 <92373617+tusharranjan007@users.noreply.github.com>
2024-11-06 20:55:38 -0500

    fix 13

--- server/src/app.ts
+++ server/src/app.ts
@@ -51,8 +51,8 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI! }),
     cookie: {
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: false, // Set to `false` temporarily for testing
+      sameSite: "lax", // Set to "lax" temporarily; switch to "none" when ready
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
   })
